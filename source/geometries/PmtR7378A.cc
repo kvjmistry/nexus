018781ab9b807e320e3812cf9f51372bb802d4bb
Ilker <ilkerparmaksiz@gmail.com>
2022-09-14 21:54:44 -0500

    Efficiencies list order changed

--- source/geometries/PmtR7378A.cc
+++ source/geometries/PmtR7378A.cc
@@ -145,27 +145,31 @@ namespace nexus {
     G4MaterialPropertiesTable* phcath_mpt = new G4MaterialPropertiesTable();
     FileHandling *f1=new FileHandling();
     std::vector<std::vector<G4double>> t;
+    /// Note: In order this efficiency to work , you need to list it from low energy to high energy
     t=f1->GetData("data/PMTR7378A_Efficiency.txt",',',1);
+    //t=f1->GetData("data/test.txt",',',1);
     const G4int entries = t[0].size();
     G4double ENERGIES[entries];
     G4double EFFICIENCY[entries];
     G4double REFLECTIVITY[entries];
-    for(int kk=0;kk<t[0].size();kk++){
+
+  for(int kk=0;kk<t[0].size();kk++){
 
         ENERGIES[kk]=t[0][kk]*eV;
         EFFICIENCY[kk]=t[1][kk];
         REFLECTIVITY[kk]=0.;
-
+        G4cout<<ENERGIES[kk]<<" "<<EFFICIENCY[kk]<<G4endl;
     }
 
-     /*    const G4int entries = 30;
-         G4double ENERGIES[entries] =
+    /*
+      const G4int entries = 31;
+      G4double ENERGIES[entries] =
       {1.72194*eV, 1.77114*eV, 1.82324*eV, 1.87848*eV, 1.93719*eV,
        1.99968*eV,  2.06633*eV, 2.13759*eV, 2.21393*eV, 2.29593*eV,
        2.38423*eV, 2.47960*eV, 2.58292*eV, 2.69522*eV, 2.81773*eV,
        2.95190*eV, 3.0995*eV, 3.26263*eV, 3.44389*eV, 3.64647*eV,
        3.87438*eV, 4.13267*eV, 4.42786*eV, 4.76846*eV, 5.16583*eV,
-       5.63545*eV, 6.19900*eV, 6.88778*eV, 7.74875*eV, 8.85571*eV};
+       5.63545*eV, 6.19900*eV, 6.88778*eV,7.08*eV, 7.74875*eV, 8.85571*eV};
 
 
        G4double EFFICIENCY[entries] =
@@ -174,7 +178,7 @@ namespace nexus {
     	0.20500, 0.23500, 0.27000, 0.29000, 0.31300,
     	0.35200, 0.38000, 0.38000, 0.37300, 0.37300,
     	0.37000, 0.36000, 0.35500, 0.33500, 0.31000,
-    	0.29500, 0.27500, 0.23000, 0.52000, 0.00000};
+    	0.29500, 0.27500, 0.23000,1, 0.52000, 0.00000};
 
        G4double REFLECTIVITY[entries] =
       { 0., 0., 0., 0., 0.,
@@ -182,13 +186,13 @@ namespace nexus {
 	0., 0., 0., 0., 0.,
 	0., 0., 0., 0., 0.,
 	0., 0., 0., 0., 0.,
-	0., 0., 0., 0., 0. };
+	0., 0., 0., 0., 0.,0 };
+
     */
 
 
-
    phcath_mpt->AddProperty("EFFICIENCY", ENERGIES, EFFICIENCY, entries);
-    phcath_mpt->AddProperty("REFLECTIVITY", ENERGIES, REFLECTIVITY, entries);
+   phcath_mpt->AddProperty("REFLECTIVITY", ENERGIES, REFLECTIVITY, entries);
 
 
 
