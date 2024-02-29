1297451cb9559d70b7757f68a99522188e88d749
Krishan Mistry <kvjmistry@gmail.com>
2024-02-28 21:54:30 -0600

    Add next100 garfield option

--- source/geometries/Next100FieldCage.cc
+++ source/geometries/Next100FieldCage.cc
@@ -42,6 +42,9 @@
 #include <G4TransportationManager.hh>
 
 #include <cassert>
+#include "GarfieldHelper.h"
+#include "DegradModel.h"
+#include "GarfieldVUVPhotonModel.h"
 
 using namespace nexus;
 
@@ -358,6 +361,15 @@ void Next100FieldCage::BuildActive()
            << " mm and ends in "  << (active_zpos_ + active_length_/2.)/mm
            << " mm" << G4endl;
   }
+
+  // Garfield stuff
+  GarfieldHelper GH(ring_ext_diam_/2.0/cm, teflon_total_length_/cm, active_diam_/2.0/cm ,active_length_/cm, 1188.1/cm, -0.1/cm, n_panels_,
+  pressure_, el_gap_length_/cm, G4ThreeVector(0., 0., active_zpos_/cm), 500, ELelectric_field_/(kilovolt/cm), 1 * mm/microsecond, 2.5 * mm/microsecond, 1000*ms, 0, 0, 1, "NEXT100");
+
+  //These commands generate the four gas models and connect it to the GasRegion
+  new DegradModel("DegradModel",drift_region, GH);
+  new GarfieldVUVPhotonModel("GarfieldVUVPhotonModel",drift_region, GH, ionisd);
+
 }
 
 
