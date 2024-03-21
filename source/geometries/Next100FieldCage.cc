3bf737c108df7cf8f72bd161c9482db9f2dc979d
Krishan Mistry <kvjmistry@gmail.com>
2024-03-21 11:16:07 -0500

    Update NExt100 drift params

--- source/geometries/Next100FieldCage.cc
+++ source/geometries/Next100FieldCage.cc
@@ -332,7 +332,7 @@ void Next100FieldCage::BuildActive()
   G4double global_active_zpos = active_zpos_ - GetCoordOrigin().z();
   field->SetCathodePosition(global_active_zpos + active_length_/2.);
   field->SetAnodePosition(global_active_zpos - active_length_/2.);
-  field->SetDriftVelocity(0.874 * mm/microsecond);
+  field->SetDriftVelocity(1.03909 * mm/microsecond);
   field->SetTransverseDiffusion(drift_transv_diff_);
   field->SetLongitudinalDiffusion(drift_long_diff_);
   field->SetLifetime(e_lifetime_);
@@ -363,8 +363,8 @@ void Next100FieldCage::BuildActive()
   }
 
   // Garfield stuff
-  GarfieldHelper GH(ring_ext_diam_/2.0/cm, teflon_drift_length_/cm, active_diam_/2.0/cm ,active_length_/cm, 1188.1/cm, -0.1/cm, n_panels_,
-  pressure_, el_gap_length_/cm, G4ThreeVector(0., 0., active_zpos_/cm), 500, ELelectric_field_/(kilovolt/cm), 0.874 * mm/microsecond, 2.5 * mm/microsecond, 1000*ms, 0, 0, 1, "NEXT100");
+  GarfieldHelper GH(ring_ext_diam_/2.0/cm, teflon_drift_length_/cm, active_diam_/2.0/cm ,active_length_/cm, 1187.5/cm, -0.127/cm, n_panels_,
+  pressure_, el_gap_length_/cm, G4ThreeVector(0., 0., active_zpos_/cm), 400, ELelectric_field_/(kilovolt/cm), 1.03909 * mm/microsecond, 4.55 * mm/microsecond, 1000*ms, 0, 0, 1, "NEXT100");
 
   //These commands generate the four gas models and connect it to the GasRegion
   new DegradModel("DegradModel",drift_region, GH);
@@ -733,7 +733,7 @@ void Next100FieldCage::BuildELRegion()
     G4double global_el_gap_zpos = el_gap_zpos_ - GetCoordOrigin().z();
     el_field->SetCathodePosition(global_el_gap_zpos + el_gap_length_/2. + grid_thickn_);
     el_field->SetAnodePosition  (global_el_gap_zpos - el_gap_length_/2. - grid_thickn_);
-    el_field->SetDriftVelocity(2.5 * mm/microsecond);
+    el_field->SetDriftVelocity(4.55 * mm/microsecond);
     el_field->SetTransverseDiffusion(ELtransv_diff_);
     el_field->SetLongitudinalDiffusion(ELlong_diff_);
     el_field->SetLightYield(XenonELLightYield(ELelectric_field_, pressure_));
